00cd612e8210279ba2b583b3e8b842c72f02fdee
Kenneth Sarah <kennethnifemi@gmail.com>
2024-01-04 23:03:37 +0100

    second commit

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,12 @@
+<template>
+    
+    <navbar />
+    <main>
+      <router-view />
+    </main>
+  
+</template>
+
+<script setup>
+    import Navbar from '../components/Navbar.vue'
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="bg-white shadow h-16 flex justify-between items-stretch">
+    <router-link
+      :to="{ name: 'home' }"
+      class="inline-flex items-center h-full px-5 text-pink-500 font-bold"
+    >
+      Home <img src="/public/recipe-logo.png" alt="" class="pl-2">
+    </router-link>
+    <div class="flex items-center gap-1">
+      <router-link
+        :to="{ name: 'byName' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-pink-400 hover:rounded-3xl hover:text-white"
+      >
+        Search Meals
+      </router-link>
+      <router-link
+        :to="{ name: 'byLetter' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-pink-400 hover:rounded-3xl hover:text-white"
+      >
+        Meals By Letter
+      </router-link>
+      <router-link
+        :to="{ name: 'ingredients' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-pink-400 hover:rounded-3xl hover:text-white"
+      >
+        Meals By Ingredients
+      </router-link>
+    </div>
+  </header>
+</template>
